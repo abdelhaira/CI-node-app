33b82e50101f5f064d7320d4565f635c8225f286
haira abd <haira.abdelhamid@gmail.com>
2019-07-06 18:46:21 +0100

    simple app

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var express = require(‘express’);
+var express = require("express");
  
 var app = express();
 
-app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
+app.get("/", function (req, res) {
+ res.send("hello world");
 });
 
-app.listen(process.env.PORT 
- 3000);
- 
+app.listen(3000, () => console.log(`app listening on port 3000!`));
+
 module.exports = app;
